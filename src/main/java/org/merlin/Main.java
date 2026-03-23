4532105a89acb3136139873cc0ef7f38bfd83e63
Merlin GUYARD <merlin.guyard@gmail.com>
2026-03-23 15:04:18 +0100

    C2

--- src/main/java/org/merlin/Main.java
+++ src/main/java/org/merlin/Main.java
@@ -3,16 +3,30 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Color;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(System.getProperty("user.dir"));
+        Properties properties = new Properties();
 
-        JFrame window = new JFrame("Hello world");
+        try (InputStream input = Main.class
+                .getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+            JFrame window = new JFrame(properties.getProperty("HELLO_WORLD"));
         window.setSize(200, 200);
         window.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
 
-        JLabel label = new JLabel("Hellow world", SwingConstants.CENTER);
+        JLabel label = new JLabel(properties.getProperty("HELLO_WORLD"), SwingConstants.CENTER);
         Color yellow = Color.yellow;
         label.setForeground(Color.yellow);
 
